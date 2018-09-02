655bef5ea3022ad731d696aecd0ee9a97ed62ca0
RobertD <robertdudus123@gmail.com>
2018-09-02 10:46:04 +0100

    making 3rd test green

--- api/canStreamLambda.js
+++ api/canStreamLambda.js
@@ -3,6 +3,7 @@ import { handler as canStream } from "./canStream";
 export const handler = async event => {
   const res = await canStream({ userId: event.pathParameters.id });
   return {
-    body: res.body && JSON.stringify(res.body)
+    body: res.body && JSON.stringify(res.body),
+    statusCode: res.statusCode
   };
 };
