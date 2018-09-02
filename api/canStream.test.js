2bf65d6fc32d2205ca1eb001a4e95724960b8007
RobertD <robertdudus123@gmail.com>
2018-09-02 11:22:29 +0100

    2nd and 3rd test passing

--- api/canStream.test.js
+++ api/canStream.test.js
@@ -5,15 +5,34 @@ import { handler } from "./canStream";
 describe("handler", () => {
   const userId = "378eb840-f70f-11e6-9d1a-1359b3b22944";
   const sandbox = sinon.createSandbox();
+  let streamServiceMockBuilder;
 
+  beforeEach(function() {
+    streamServiceMockBuilder = new StreamServiceMockBuilder(sandbox);
+  });
   afterEach(function() {
     sandbox.restore();
   });
 
   it("returns success if user is streaming 2 videos", async () => {
-    let streamServiceMockBuilder = new StreamServiceMockBuilder(sandbox);
     const streamServiceMock = streamServiceMockBuilder
       .returns(2)
+      .for(userId)
+      .build();
+    var res = await handler({ userId }, streamServiceMock);
+    expect(res.statusCode).toBe(200);
+  });
+  it("returns success if user is streaming no videos", async () => {
+    const streamServiceMock = streamServiceMockBuilder
+      .returns(0)
+      .for(userId)
+      .build();
+    var res = await handler({ userId }, streamServiceMock);
+    expect(res.statusCode).toBe(200);
+  });
+  it("returns success if user is streaming -1 videos", async () => {
+    const streamServiceMock = streamServiceMockBuilder
+      .returns(-1)
       .for(userId)
       .build();
     var res = await handler({ userId }, streamServiceMock);
