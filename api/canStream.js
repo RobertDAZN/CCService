1e8543bb011bab96e118fc45fd9cf86b5d260d24
RobertD <robertdudus123@gmail.com>
2018-09-02 12:11:15 +0100

    9th test passing

--- api/canStream.js
+++ api/canStream.js
@@ -2,29 +2,35 @@ import { settings } from "../configuration/settings";
 import { errorNumbers } from "../configuration/errorNumbers";
 
 export const handler = async (event, streamService) => {
-  if (!event.userId) {
-    return { statusCode: 400 };
-  }
-  const streamServiceResponse = await streamService.getNumberOfVideosBeingWatched(
-    event.userId
-  );
-  if (streamServiceResponse.errorNumber) {
-    switch (streamServiceResponse.errorNumber) {
-      case 401:
-        return { statusCode: 401 };
-      default:
-        return { statusCode: 500 };
+  try {
+    if (!event.userId) {
+      return { statusCode: 400 };
     }
-  }
-  if (
-    streamServiceResponse.numberOfVideosBeingWatched < settings.streamsLimit
-  ) {
+    const streamServiceResponse = await streamService.getNumberOfVideosBeingWatched(
+      event.userId
+    );
+    if (streamServiceResponse.errorNumber) {
+      switch (streamServiceResponse.errorNumber) {
+        case 401:
+          return { statusCode: 401 };
+        default:
+          return { statusCode: 500 };
+      }
+    }
+    if (
+      streamServiceResponse.numberOfVideosBeingWatched < settings.streamsLimit
+    ) {
+      return {
+        statusCode: 200
+      };
+    }
     return {
-      statusCode: 200
+      statusCode: 200,
+      body: { errorNumber: errorNumbers.NumberOfStreamsExceeded }
+    };
+  } catch (error) {
+    return {
+      statusCode: 500
     };
   }
-  return {
-    statusCode: 200,
-    body: { errorNumber: errorNumbers.NumberOfStreamsExceeded }
-  };
 };
