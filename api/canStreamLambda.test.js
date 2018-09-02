c743786773c8e42a2a0a177d60c7b216d5362bff
RobertD <robertdudus123@gmail.com>
2018-09-02 10:30:18 +0100

    filling in first failing test

--- api/canStreamLambda.test.js
+++ api/canStreamLambda.test.js
@@ -1,5 +1,29 @@
+import * as sinon from "sinon";
+import * as canStream from "./canStream";
+import * as lambda from "./canStreamLambda";
+
 describe("handler", () => {
+  const sandbox = sinon.createSandbox();
+  const userId = "378eb840-f70f-11e6-9d1a-1359b3b22944";
+  let canStreamMock;
+
+  beforeEach(function() {
+    canStreamMock = sandbox.stub(canStream, "handler");
+  });
+  afterEach(function() {
+    sandbox.restore();
+  });
+
   it("calls internal handler with valid arguments", async () => {
-    throw new Error("Not implemented");
+    const event = {
+      pathParameters: {
+        id: userId
+      }
+    };
+    canStreamMock.returns({});
+
+    await lambda.handler(event);
+
+    sinon.assert.calledWith(canStreamMock, { userId: event.pathParameters.id });
   });
 });
